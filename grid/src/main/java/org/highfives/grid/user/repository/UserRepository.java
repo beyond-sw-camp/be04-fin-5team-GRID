72e962789bd12aa045d13153d3ec882a538480d1
동동 <baek0810@naver.com>
2024-05-16 12:24:00 +0900

    Feat: 휴가기능 구현을 위한 직원 코드 추가

---
+++ grid/src/main/java/org/highfives/grid/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.highfives.grid.user.repository;
+
+import org.highfives.grid.user.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Employee, Long> {
+}
