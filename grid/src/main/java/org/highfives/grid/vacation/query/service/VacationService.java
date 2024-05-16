27c4618268c275e48d73db21300f24d785540477
동동 <baek0810@naver.com>
2024-05-16 12:12:14 +0900

    Feat: 휴가관련 검색기능 추가

--- grid/src/main/java/org/highfives/grid/vacation/query/service/VacationService.java
+++ grid/src/main/java/org/highfives/grid/vacation/query/service/VacationService.java
@@ -14,4 +14,10 @@ public interface VacationService {
     List<VacationPolicyDTO> getVacationPolicy(int typeId);
 
     List<VacationHistoryDTO> getAllVacationHistory();
+
+    List<VacationInfoDTO> searchVacationInfoByName(String name);
+
+    List<VacationInfoDTO> searchVacationInfoByDept(String dept);
+
+    List<VacationHistoryDTO> searchVacationHistory(int typeId, int changeTypeId, int employeeId);
 }
---
+++ grid/src/main/java/org/highfives/grid/vacation/query/vo/HistorySearch.java
@@ -0,0 +1,7 @@
+package org.highfives.grid.vacation.query.vo;
+
+public class HistorySearch {
+    private int typeId;
+    private int changeTypeId;
+    private int employeeId;
+}
