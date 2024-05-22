9e4add011441cb3d004d88eeb193b36c4d19f1ea
동동 <baek0810@naver.com>
2024-05-22 11:02:51 +0900

    Fix: 필요없는 파일 삭제

--- grid/src/main/java/org/highfives/grid/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.highfives.grid.user.repository;
-
-import org.highfives.grid.user.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Employee, Integer> {
-}
--- grid/src/main/java/org/highfives/grid/user/vo/UserAnnualResponse.java
+++
@@ -1,6 +0,0 @@
-package org.highfives.grid.user.vo;
-
-public class UserAnnualResponse {
-    private int id;
-    private int Annual;
-}
