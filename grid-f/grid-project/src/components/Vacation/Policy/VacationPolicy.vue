cea74e3ee36d4cf9f39c662c029e1626303e9bd5
동동 <baek0810@naver.com>
2024-05-23 17:36:13 +0900

    Feat: 휴가 정책관리 화면 구현

--- grid-f/grid-project/src/components/Vacation/Policy/VacationPolicy.vue
+++ grid-f/grid-project/src/components/Vacation/Policy/VacationPolicy.vue
@@ -1,151 +1,132 @@
 <template>
     <div class="policyAll">
         <div class="policyTitle">
-            <img class = "policyIcon" src="@/assets/buttons/policy.png">
+            <img class="policyIcon" src="@/assets/buttons/vacation.png">
             <h1>휴가정책</h1>
-            <button class = "policyRegist" @click="registPolicy()">등록하기</button>
+            <button class="policyRegist" @click="registPolicy()">등록하기</button>
         </div>
         <div class="policyContent">
-            <div class="policyBox">
+            <div class="policyBox" v-for="policy in policies" :key="policy.id">
                 <div class="policyHeader">
-                    <h3 class="policyName">연차</h3>
-                    <img class="policyModify" src="@/assets/buttons/edit.png" @click="modifyPolicy()">
+                    <h3 class="policyName">{{ policy.typeName }}</h3>
+                    <img class="policyModify" src="@/assets/buttons/edit.png" @click="modifyPolicy(policy.id)">
                 </div>
-                <h5 class="policies">근로기준법 제 60조(연차 유급휴가)
-                    <br> 1. 사용자는 1년간 80퍼센트 이상 출근한 근로자에게 15일의 유급휴가를 주어야 한다. <개정 2012. 2. 1.>
-                    <br> 2. 사용자는 계속하여 근로한 기간이 1년 미만인 근로자 또는 1년간 80퍼센트 미만 출근한 근로자에게 1개월 개근 시 1일의 유급휴가를 주어야 한다. <개정 2012. 2. 1.> 
-                    <br> 3. 사용자는 3년 이상 계속하여 근로한 근로자에게는 제1항에 따른 휴가에 최초 1년을 초과하는 계속 근로 연수 매 2년에 대하여 1일을 가산한 유급휴가를 주어야 한다.
-                    <br> 이 경우 가산휴가를 포함한 총 휴가 일수는 25일을 한도로 한다.
-                    <br> 4. 사용자는 제1항부터 제4항까지의 규정에 따른 휴가를 근로자가 청구한 시기에 주어야 하고, 그 기간에 대하여는 취업규칙 등에서 정하는 통상임금 또는 평균임금을
-                    <br> 지급하여야 한다. 다만, 근로자가 청구한 시기에 휴가를 주는 것이 사업 운영에 막대한 지장이 있는 경우에는 그 시기를 변경할 수 있다.
-                    <br> 5. 제1항 및 제2항을 적용하는 경우 다음 각 호의 어느 하나에 해당하는 기간은 출근한 것으로 본다. <개정 2012. 2. 1., 2017. 11. 28.>
-                    <br>
-                    <br>  &nbsp; 1. 근로자가 업무상의 부상 또는 질병으로 휴업한 기간
-                    <br>  &nbsp; 2. 임신 중의 여성이 제74조제1항부터 제3항까지의 규정에 따른 휴가로 휴업한 기간
-                    <br>  &nbsp; 3. 「남녀고용평등과 일ㆍ가정 양립 지원에 관한 법률」 제19조제1항에 따른 육아휴직으로 휴업한 기간
-                    <br>
-                    <br> 6. 제1항ㆍ제2항 및 제4항에 따른 휴가는 1년간(계속하여 근로한 기간이 1년 미만인 근로자의 제2항에 따른 유급휴가는 최초 1년의 근로가 끝날 때까지의 기간을 말한다) 
-                    <br> 행사하지 아니하면 소멸된다. 다만, 사용자의 귀책사유로 사용하지 못한 경우에는 그러하지 아니하다. <개정 2020. 3. 31.>    
-                </h5>
+                <h5 class="policies" v-html="policy.content"></h5>
             </div>
-            <div class="policyBox">
-                <div class="policyHeader">
-                    <h3 class="policyName">보건 휴가</h3>
-                    <img class="policyModify" src="@/assets/buttons/edit.png">
-                </div>
-                <h5 class="policies">1. 여성 직원은 매달 1일 보건휴가 1일이 자동으로 추가된다.
-                    <br> 2. 사용하지 않은 보건휴가는 매달  말일 자동으로 소멸된다.
-                </h5>
-            </div>
-            <div class="policyBox">
-                <div class = "policyHeader">
-                    <h3 class="policyName">정기 휴가</h3>
-                    <img class="policyModify" src="@/assets/buttons/edit.png">
-                </div>
-                
-                <h5 class="policies">1. 정기휴가는 입사이후 1년부터 기본 4일이 제공된다.
-                    <br> 2. 받은후로부터 1년안에 사용해야하며, 사용하지 않은 정기휴가는 소멸되고 따로 보상받을 수 없다. 
-                </h5>
-            </div>
-        
         </div>
-    
-
     </div>
-    
 </template>
 
 <script setup>
 import router from '@/router/router';
+import { onBeforeMount, ref } from 'vue';
+import axios from "axios";
 
-function modifyPolicy() {
-    router.push('/vacation/policy/modify');
+const policies = ref([]);
+
+const getAllVacationPolicy = async () => {
+    try {
+        const response = await axios.get("/api/vacation/policy/all");
+        policies.value = response.data.result;
+        console.log(response.data.result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+function modifyPolicy(id) {
+    router.push(`/vacation/policy/modify/${id}`);
 }
 
 function registPolicy() {
     router.push('/vacation/policy/regist');
 }
 
+onBeforeMount(() => {
+    getAllVacationPolicy();
+});
 </script>
 
 <style scoped>
-    
+.policyAll {
+    display: grid;
+    grid-template-rows: 18% minmax(80%,auto) 2%;
+    grid-template-columns: 10% 80% 10%;
+    height: 100%;
+}
 
-    .policyAll {
-        display: grid;
-        grid-template-rows: 15% 85%;
-        grid-template-columns: 5% 85% 10%;
-        height: 100%;
-    }
+.policyTitle {
+    grid-column-start: 2;
+    grid-column-end: 3;
+    font-size: 12px;
+    font-weight: 600;
+    margin-top: 2%;
+    color: #000000;
+    display: grid;
+    grid-template-columns: 3% 92% 5%;
+    align-items: center;
+}
 
-    .policyTitle {
-        grid-column-start: 2;
-        grid-column-end: 3;
-        font-size: 20px;
-        font-weight: 600;
-        margin-top: 1%;
-        color: #000000;
-        display: grid;
-        grid-template-columns: 5% 88% 7%;
-        align-items: center;
-    }
+.policyTitle h1 {
+    margin-left:0.5%;
+}
 
-    .policyName {
-        margin-top: 0;
-        margin-bottom: 2%;
-        font-size: 20px;
-        grid-column-start: 1;
-        grid-column-end: 2;
-    }
+.policyIcon {
+    width: 80%;
+}
 
-    .policyContent {
-        margin-top: 2%;
-        margin-bottom: 0;
-        grid-column-start: 2;
-        grid-column-end: 3;
-    }
+.policyName {
+    margin-top: 0;
+    margin-bottom: 0;
+    font-size: 15px;
+    grid-column-start: 1;
+    grid-column-end: 2;
+}
 
-    .policies {
-        margin-top: 0;
-        margin-bottom: 0;
-    }
+.policyContent {
+    margin-top: 2%;
+    margin-bottom: 0;
+    grid-column-start: 2;
+    grid-column-end: 3;
+    font-size: 15px;
+}
 
-    .policyBox {
-        width: calc(100% - 20px); /* 부모 요소의 너비에 비례하여 폭을 설정 */
-        padding: 10px; /* 내부 여백 추가 */
-        margin-top: 10px;
-        margin-bottom: 0;
-        background-color: #F8F9FAFF;
-    }
+.policies {
+    margin-top: 0;
+    margin-bottom: 0;
+    white-space: pre-wrap; /* 공백과 줄 바꿈을 유지 */
+}
 
-    .policyIcon {
-        width:100%;
+.policyBox {
+    width: calc(100% - 20px); /* 부모 요소의 너비에 비례하여 폭을 설정 */
+    padding: 10px; /* 내부 여백 추가 */
+    margin-top: 10px;
+    margin-bottom: 0;
+    background-color: #F2F2F2;
+}
 
-    }
+.policyRegist {
+    background-color: #088A85;
+    color: white;
+    padding: 5px 5px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 11px;
+    font-style: bold;
+    width: 100%;
+}
 
-    .policyRegist {
-        background-color: orange;
-        color: white;
-        padding: 10px 10px;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        font-size: 12px;
-        font-style: bold;
-    }
+.policyHeader {
+    display: grid;
+    grid-template-columns: 97% 3%;
+    align-items: center;
+    margin-bottom: 2%;
+}
 
-    .policyHeader {
-        display: grid;
-        grid-template-columns: auto auto;
-        align-items: center;
-    }
-
-    .policyModify {
-        justify-self: end;
-        cursor: pointer;
-        width: 30px; /* 크기를 적절하게 조절 */
-    }
-    
-
-    
+.policyModify {
+    justify-self: end;
+    cursor: pointer;
+    width: 70%;
+}
 </style>
--- grid-f/grid-project/src/views/VacationPolicyView.vue
+++ grid-f/grid-project/src/views/VacationPolicyView.vue
@@ -1,7 +1,16 @@
 <template>
     
-    <Policy />
-
+    
+    <div class="all">
+      <div class="header">
+          <RouterLink to="/vacation/history" replace>history</RouterLink>
+          <RouterLink to="/vacation/policy" replace>policy</RouterLink>
+          <RouterLink to="/vacation/manage" replace>manage</RouterLink>
+          <RouterLink to="/vacation/info" replace>info</RouterLink>
+      </div>
+      <div class="sidebar">Sidebar</div>
+      <div class="content"><Policy /></div>
+</div>
 </template>
 
 <script setup>
@@ -10,5 +19,27 @@
 </script>
 
 <style scoped>
+  .all {
+    display: grid;
+    grid-template-rows: 100px minmax(764px, auto);
+    grid-template-columns: 250px minmax(1286px,auto);
+    width: 100%;
+    height: 100vh;
+  }
 
+  .header {
+      grid-column-start: 1;
+      grid-column-end: 3;
+      background-color: #088A85;
+  }
+
+  .sidebar {
+      grid-row: 2;
+      background-color: #088A85;
+  }
+
+  .content {
+     grid-row: 2;
+      background-color: white;
+  }
 </style>
