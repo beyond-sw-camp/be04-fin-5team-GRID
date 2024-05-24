cf1bf23f2507294e8c06efe58ecd7e36bbd2dcd4
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-05-24 18:08:44 +0900

    Feat: 휴가종류 관리페이지 구현
    메인화면, 수정,삭제,등록 페이지 구현

--- grid-f/grid-project/src/components/Vacation/Manage/VacationManageDelete.vue
+++ grid-f/grid-project/src/components/Vacation/Manage/VacationManageDelete.vue
@@ -6,37 +6,95 @@
         </div>
         <div class="deleteMain">
             <div class="vacationType">
-                <p>휴가 종류</p>
-                <input type="text" placeholder="휴가 종류를 입력해주세요." class="inputField"/>
+                <h3>휴가 종류</h3>
+                <p class="titleContent">{{ type.typeName }}</p>
             </div>
             <div class="vacationGiveNum">
-                <p>휴가 제공일수</p>
-                <input type="text" placeholder="휴가 제공일수를 입력해주세요." class="inputField"/>
+                <h3>휴가 제공일수</h3>
+                <textarea v-model="type.vacationNum"></textarea>
             </div>
             <div class="vacationPeriod">
-                <p>휴가 사용기한</p>
-                <input type="text" placeholder="휴가 사용기한을 입력해주세요." class="inputField"/>
+                <h3>휴가 사용기한</h3>
+                <textarea v-model="type.dateOfUse"></textarea>
             </div>
             <div class="vacationDesc">
-                <p>설명</p>
-                <input type="text" placeholder="설명을 입력해주세요." class="inputField"/>
+                <h3>설명</h3>
+                <textarea v-model="type.vacationExplain"></textarea>
             </div>
         </div>
         <div class="buttons">
-                <button class="modify">수정하기</button>
-                <button class="delete">삭제하기</button>
+                <button class="modify" @click="modifyVacationType">수정하기</button>
+                <button class="delete" @click="deleteVacationType">삭제하기</button>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue';
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+import router from '@/router/router';
+
+const type = ref({
+    typeName: '',
+    vacationNum: '',
+    dateOfUse: '',
+    vacationExplain: ''
+});
+
+const route = useRoute();
+
+const getVacationType = async () => {
+    try {
+        const response = await axios.get(`/api/vacation/type/${route.params.id}`);
+        type.value = response.data.result;
+        console.log(response.data.result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+const modifyVacationType = async () => {
+    try {
+        const confirmed = window.confirm('수정하시겠습니까?');
+        if (confirmed) {
+            const response = await axios.put(`/api/vacation/type/${route.params.id}`, 
+            { 
+                vacationNum: type.value.vacationNum, 
+                dateOfUse: type.value.dateOfUse, 
+                vacationExplain: type.value.vacationExplain 
+            });
+            alert('수정이 완료되었습니다.');
+            window.location.reload();
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+const deleteVacationType = async () => {
+    try {
+        const confirmed = window.confirm('정말로 삭제하시겠습니까?');
+        if(confirmed) {
+            const response = await axios.delete(`/api/vacation/type/${route.params.id}`);
+            alert('삭제가 완료되었습니다.');
+            router.go(-1);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+onBeforeMount(() => {
+    getVacationType();
+});
 
 </script>
 
 <style scoped>
     .manageDeleteAll {
         display: grid;
-        grid-template-rows: 15% 80% 5% ;
+        grid-template-rows: 18% 70% 12% ;
         grid-template-columns: 10% 80% 10%;
         height: 100%;
     }
@@ -44,74 +102,78 @@
     .manageTitle {
         grid-column-start: 2;
         grid-column-end: 3;
-        font-size: 20px;
+        font-size: 12px;
         font-weight: 600;
-        margin-top: 1%;
+        margin-top: 2%;
         color: #000000;
         display: grid;
-        grid-template-columns: 5% 95%;
+        grid-template-columns: 3% 97%;
         align-items: center;
     }
 
     .deleteIcon {
-        width:100%;
+        width:80%;
     }
 
     .deleteMain {
-        margin-top: 4%;
+        margin-top: 3%;
         margin-bottom: 2%;
-        font-size: 20px;
         grid-column-start: 2;
         grid-column-end: 3;
-        height: 80%;
+        height: 70%;
         width: calc(100% - 20px); /* 부모 요소의 너비에 비례하여 폭을 설정 */
         padding: 10px; /* 내부 여백 추가 */
-        background-color: #F8F9FAFF;
+        background-color: #F2F2F2;
     }
 
     .vacationType {
         margin-top: 2%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        align-items: center;
+        font-size: 14px;
 
     }
 
     .vacationGiveNum {
-        margin-top: 3%;
+        margin-top: 2%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
     }
 
     .vacationPeriod {
         margin-top: 2%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
 
     }
 
     .vacationDesc {
-        margin-top: 3%;
-        height: 50%;
+        margin-top: 2%;
+        height: 40%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
+    }
+
+    .vacationDesc h3 {
+        margin-top: 0;
     }
 
     .buttons {
         display: grid;
-        grid-template-columns: 30% 9% 10% 2% 10% 9% 30%;
+        grid-template-columns: 40% 8% 4% 8% 40%;
         place-items: center;
         grid-row-start: 3;
         grid-column-start: 2;
     }
 
     .delete {
-        grid-column-start: 5;
+        grid-column-start: 4;
         width: 100%;
-        background-color: orange;
+        background-color: #088A85;
         color: white;
         padding: 10px 10px;
         border: none;
@@ -122,9 +184,9 @@
     }
 
     .modify {
-        grid-column-start: 3;
+        grid-column-start: 2;
         width: 100%;
-        background-color: orange;
+        background-color: #088A85;
         color: white;
         padding: 10px 10px;
         border: none;
@@ -137,4 +199,39 @@
     .inputField {
         padding-left: 10px; 
     } 
+
+    .titleContent {
+        background-color: #F8F9FAFF;
+        padding: 10px;
+    }
+
+    .vacationGiveNum textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 50px; /* 고정 높이 설정 */
+        border: none;
+    }
+
+    .vacationPeriod textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 50px; /* 고정 높이 설정 */
+        border: none;
+    }
+
+    .vacationDesc textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 100px; /* 고정 높이 설정 */
+        border: none;
+    }
 </style>
--- grid-f/grid-project/src/components/Vacation/Manage/VacationManageRegist.vue
+++ grid-f/grid-project/src/components/Vacation/Manage/VacationManageRegist.vue
@@ -6,36 +6,63 @@
         </div>
         <div class="registMain">
             <div class="vacationType">
-                <p>휴가 종류</p>
-                <input type="text" placeholder="휴가 종류를 입력해주세요." class="inputField"/>
+                <h3>휴가 종류</h3>
+                <textarea v-model="typeName"></textarea>
             </div>
             <div class="vacationGiveNum">
-                <p>휴가 제공일수</p>
-                <input type="text" placeholder="휴가 제공일수를 입력해주세요." class="inputField"/>
+                <h3>휴가 제공일수</h3>
+                <textarea v-model="vacationNum"></textarea>
             </div>
             <div class="vacationPeriod">
-                <p>휴가 사용기한</p>
-                <input type="text" placeholder="휴가 사용기한을 입력해주세요." class="inputField"/>
+                <h3>휴가 사용기한</h3>
+                <textarea v-model="dateOfUse"></textarea>
             </div>
             <div class="vacationDesc">
-                <p>설명</p>
-                <input type="text" placeholder="설명을 입력해주세요." class="inputField"/>
+                <h3>설명</h3>
+                <textarea v-model="vacationExplain"></textarea>
             </div>
         </div>
         <div class="buttons">
-                <button class="regist">등록하기</button>
+            <button class="regist" @click="registType">등록하기</button>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue';
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+import router from '@/router/router';
 
+const typeName = ref('');
+const vacationNum = ref('');
+const dateOfUse = ref('');
+const vacationExplain = ref('');
+
+const route = useRoute();
+
+const registType = async () => {
+    try {
+        const response = await axios.post("/api/vacation/type",
+        {
+            typeName: typeName.value,
+            vacationNum: vacationNum.value,
+            dateOfUse: dateOfUse.value,
+            vacationExplain: vacationExplain.value
+        });
+        alert('등록이 완료되었습니다.');
+        window.location.reload();
+        router.go(-1); // 이전 화면으로 이동
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
 </script>
 
 <style scoped>
     .manageRegistAll {
         display: grid;
-        grid-template-rows: 15% 80% 5% ;
+        grid-template-rows: 18% 70% 12% ;
         grid-template-columns: 10% 80% 10%;
         height: 100%;
     }
@@ -43,65 +70,108 @@
     .manageTitle {
         grid-column-start: 2;
         grid-column-end: 3;
-        font-size: 20px;
+        font-size: 12px;
         font-weight: 600;
-        margin-top: 1%;
+        margin-top: 2%;
         color: #000000;
         display: grid;
-        grid-template-columns: 5% 95%;
+        grid-template-columns: 3% 97%;
         align-items: center;
     }
 
     .registIcon {
-        width:100%;
+        width:80%;
     }
 
     .registMain {
-        margin-top: 4%;
+        margin-top: 3%;
         margin-bottom: 2%;
-        font-size: 20px;
         grid-column-start: 2;
         grid-column-end: 3;
-        height: 80%;
+        height: 70%;
         width: calc(100% - 20px); /* 부모 요소의 너비에 비례하여 폭을 설정 */
         padding: 10px; /* 내부 여백 추가 */
-        background-color: #F8F9FAFF;
+        background-color: #F2F2F2;
+    }
+
+    .vacationType textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 50px; /* 고정 높이 설정 */
+        border: none;
     }
 
     .vacationType {
         margin-top: 2%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
 
     }
 
+    .vacationGiveNum textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 50px; /* 고정 높이 설정 */
+        border: none;
+    }
+
+    .vacationPeriod textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 50px; /* 고정 높이 설정 */
+        border: none;
+    }
+
+    .vacationDesc textarea {
+        width: 100%;
+        height: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        resize: none; /* textarea 크기 변경 비활성화 */
+        height: 100px; /* 고정 높이 설정 */
+        border: none;
+    }
+
     .vacationGiveNum {
-        margin-top: 3%;
+        margin-top: 2%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
     }
 
     .vacationPeriod {
         margin-top: 2%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
 
     }
 
     .vacationDesc {
         margin-top: 3%;
-        height: 50%;
+        height: 40%;
         display: grid;
-        grid-template-columns: 8% 92%;
-        font-size: 15px;
+        grid-template-columns: 10% 90%;
+        font-size: 14px;
+    }
+
+    .vacationDesc h3 {
+        margin-top: 0;
     }
 
     .buttons {
         display: grid;
-        grid-template-columns: 40% 5% 10% 5% 40%;
+        grid-template-columns: 40% 6% 8% 6% 40%;
         place-items: center;
         grid-row-start: 3;
         grid-column-start: 2;
@@ -110,7 +180,7 @@
     .regist {
         grid-column-start: 3;
         width: 100%;
-        background-color: orange;
+        background-color: #088A85;
         color: white;
         padding: 10px 10px;
         border: none;
