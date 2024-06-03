4af46ad38a77c1d8d8b66fda72b276eec5772406
t4e1 <jtw5738@naver.com>
2024-06-03 17:30:22 +0900

    Fix : 프론트 node_modules & package-lock.json 삭제 후 업로드

--- grid-f/grid-project/vite.config.js
+++ grid-f/grid-project/vite.config.js
@@ -6,10 +6,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  esbuild: {
-    target: 'esnext',
-    platform: 'linux',
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
