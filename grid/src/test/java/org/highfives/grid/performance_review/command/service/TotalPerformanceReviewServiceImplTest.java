fc5ead67196be37ae49d49b476726dead3eb0a01
LeeYewon <onelee521@gmail.com>
2024-06-04 09:23:36 +0900

    Fix: 테스트 케이스 주석
    아직 더미 데이터 생성 전이라 주석 처리함

--- grid/src/test/java/org/highfives/grid/performance_review/command/service/TotalPerformanceReviewServiceImplTest.java
+++ grid/src/test/java/org/highfives/grid/performance_review/command/service/TotalPerformanceReviewServiceImplTest.java
@@ -1,59 +1,59 @@
-package org.highfives.grid.performance_review.command.service;
-
-import org.highfives.grid.performance_review.command.dto.PerformanceReviewDTO;
-import org.highfives.grid.performance_review.command.dto.TotalPerformanceReviewDTO;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class TotalPerformanceReviewServiceImplTest {
-
-    @Autowired
-    private TotalPerformanceReviewServiceImpl totalPerformanceReviewService;
-
-    @DisplayName("종합 평가 추가")
-    @Test
-    @Transactional
-    void addTotalReview() {
-        List<PerformanceReviewDTO> performanceReviewDTOList = new ArrayList<>();
-
-        performanceReviewDTOList.add(new PerformanceReviewDTO(
-                3,
-                "M",
-                2024,
-                "2024년 인사평가",
-                "V",
-                12,
-                "2024-06-01 12:00:00",
-                10,
-                "2024-06-03 12:00:00",
-                2
-        ));
-
-        performanceReviewDTOList.add(new PerformanceReviewDTO(
-                4,
-                "F",
-                2024,
-                "2023년 인사평가",
-                "V",
-                12,
-                "2024-12-01 12:00:00",
-                10,
-                "2024-12-02 12:00:00",
-                2
-        ));
-
-        TotalPerformanceReviewDTO total = totalPerformanceReviewService.addTotalReview(performanceReviewDTOList);
-
-        System.out.println(total);
-        assertNotNull(total);
-    }
-}
+//package org.highfives.grid.performance_review.command.service;
+//
+//import org.highfives.grid.performance_review.command.dto.PerformanceReviewDTO;
+//import org.highfives.grid.performance_review.command.dto.TotalPerformanceReviewDTO;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class TotalPerformanceReviewServiceImplTest {
+//
+//    @Autowired
+//    private TotalPerformanceReviewServiceImpl totalPerformanceReviewService;
+//
+//    @DisplayName("종합 평가 추가")
+//    @Test
+//    @Transactional
+//    void addTotalReview() {
+//        List<PerformanceReviewDTO> performanceReviewDTOList = new ArrayList<>();
+//
+//        performanceReviewDTOList.add(new PerformanceReviewDTO(
+//                3,
+//                "M",
+//                2024,
+//                "2024년 인사평가",
+//                "V",
+//                12,
+//                "2024-06-01 12:00:00",
+//                10,
+//                "2024-06-03 12:00:00",
+//                2
+//        ));
+//
+//        performanceReviewDTOList.add(new PerformanceReviewDTO(
+//                4,
+//                "F",
+//                2024,
+//                "2023년 인사평가",
+//                "V",
+//                12,
+//                "2024-12-01 12:00:00",
+//                10,
+//                "2024-12-02 12:00:00",
+//                2
+//        ));
+//
+//        TotalPerformanceReviewDTO total = totalPerformanceReviewService.addTotalReview(performanceReviewDTOList);
+//
+//        System.out.println(total);
+//        assertNotNull(total);
+//    }
+//}
--- grid/src/test/java/org/highfives/grid/performance_review/query/service/PerformanceReviewGoalServiceImplTest.java
+++ grid/src/test/java/org/highfives/grid/performance_review/query/service/PerformanceReviewGoalServiceImplTest.java
@@ -1,63 +1,63 @@
-package org.highfives.grid.performance_review.query.service;
-
-import org.highfives.grid.performance_review.query.dto.DetailGoalDTO;
-import org.highfives.grid.performance_review.query.dto.PerformanceReviewGoalDTO;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class PerformanceReviewGoalServiceImplTest {
-
-    @Autowired
-    private PerformanceReviewGoalServiceImpl performanceReviewGoalService;
-
-    @DisplayName("11번 직원의 업적 평가 목표 목록 조회")
-    @Test
-    public void findGoalByWriterId(){
-        int employeeId = 11;
-        List<PerformanceReviewGoalDTO> findGoalList = performanceReviewGoalService.findAllGoalByWriterId(employeeId);
-
-        System.out.println(findGoalList);
-        assertNotNull(findGoalList);
-    }
-
-    @DisplayName("10번 직원이 결재할 업적 평가 목록 조회")
-    @Test
-    public void findGoalByEApproverId(){
-        int employeeId = 10;
-        List<PerformanceReviewGoalDTO> findGoalList = performanceReviewGoalService.findAllGoalByApproverId(employeeId);
-
-        System.out.println(findGoalList);
-        assertNotNull(findGoalList);
-    }
-
-    @DisplayName("1번 평가 목표 상세 조회")
-    @Test
-    public void findDetailGoalById(){
-        int id = 1;
-        DetailGoalDTO findGoal = performanceReviewGoalService.findDetailGoalById(id);
-
-        assertNotNull(findGoal);
-    }
-
-
-    @DisplayName("11번 직원의 2024 업적 평가 목표 조회")
-    @Test
-    public void findGoalByWriterIdAndYear(){
-        int writerId = 11;
-
-        int year = 2024;
-
-        PerformanceReviewGoalDTO performanceReviewGoalDTO = performanceReviewGoalService.findGoalByWriterIdAndYear(writerId, year);
-
-        System.out.println(performanceReviewGoalDTO);
-
-        assertNotNull(performanceReviewGoalDTO);
-    }
-}
+//package org.highfives.grid.performance_review.query.service;
+//
+//import org.highfives.grid.performance_review.query.dto.DetailGoalDTO;
+//import org.highfives.grid.performance_review.query.dto.PerformanceReviewGoalDTO;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class PerformanceReviewGoalServiceImplTest {
+//
+//    @Autowired
+//    private PerformanceReviewGoalServiceImpl performanceReviewGoalService;
+//
+//    @DisplayName("11번 직원의 업적 평가 목표 목록 조회")
+//    @Test
+//    public void findGoalByWriterId(){
+//        int employeeId = 11;
+//        List<PerformanceReviewGoalDTO> findGoalList = performanceReviewGoalService.findAllGoalByWriterId(employeeId);
+//
+//        System.out.println(findGoalList);
+//        assertNotNull(findGoalList);
+//    }
+//
+//    @DisplayName("10번 직원이 결재할 업적 평가 목록 조회")
+//    @Test
+//    public void findGoalByEApproverId(){
+//        int employeeId = 10;
+//        List<PerformanceReviewGoalDTO> findGoalList = performanceReviewGoalService.findAllGoalByApproverId(employeeId);
+//
+//        System.out.println(findGoalList);
+//        assertNotNull(findGoalList);
+//    }
+//
+//    @DisplayName("1번 평가 목표 상세 조회")
+//    @Test
+//    public void findDetailGoalById(){
+//        int id = 1;
+//        DetailGoalDTO findGoal = performanceReviewGoalService.findDetailGoalById(id);
+//
+//        assertNotNull(findGoal);
+//    }
+//
+//
+//    @DisplayName("11번 직원의 2024 업적 평가 목표 조회")
+//    @Test
+//    public void findGoalByWriterIdAndYear(){
+//        int writerId = 11;
+//
+//        int year = 2024;
+//
+//        PerformanceReviewGoalDTO performanceReviewGoalDTO = performanceReviewGoalService.findGoalByWriterIdAndYear(writerId, year);
+//
+//        System.out.println(performanceReviewGoalDTO);
+//
+//        assertNotNull(performanceReviewGoalDTO);
+//    }
+//}
