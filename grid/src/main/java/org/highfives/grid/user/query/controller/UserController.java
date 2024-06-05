c7c0a742413d93a2178fd41691f1cb58c0703ac4
t4e1 <jtw5738@naver.com>
2024-06-05 12:47:59 +0900

    TEST

--- grid/src/main/java/org/highfives/grid/user/query/controller/UserController.java
+++ grid/src/main/java/org/highfives/grid/user/query/controller/UserController.java
@@ -220,6 +220,6 @@ public class UserController {
     @GetMapping("/health")
     public String healthCheck() {
 
-        return "Health check success test333";
+        return "Health check success test3334";
     }
 }
