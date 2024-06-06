ef322408fc0662d495eca202a4161a53a0658c63
t4e1 <jtw5738@naver.com>
2024-06-06 11:44:16 +0900

    ㄹ

--- grid/src/main/java/org/highfives/grid/security/JwtUtil.java
+++ grid/src/main/java/org/highfives/grid/security/JwtUtil.java
@@ -187,8 +187,8 @@ public class JwtUtil {
         cookie.setMaxAge(24*60*60);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
-        cookie.setDomain("www.gridhr.site");
-
+        cookie.setDomain("grid-frontend-env-1.eba-xymvvqgw.ap-northeast-2.elasticbeanstalk.com");
+        cookie.setSecure(false); // HTTPS를 사용하지 않으므로 false
         return cookie;
     }
 }
