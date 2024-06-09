838f366701ce88422fba6d9e78f0498ce7d65b41
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-06-10 04:37:55 +0900

    Fix: 폰트 수정

--- grid-f/grid-project/src/views/Approval/ApprovalDetailView.vue
+++ grid-f/grid-project/src/views/Approval/ApprovalDetailView.vue
@@ -43,13 +43,13 @@
   <div class="all">
   <div class="detail">
     <div>
-      <h3 class="fw-bolder mb-3"><i class="bi bi-file-earmark-text"></i>&nbsp; 결재 문서</h3>
-      <b-card class="container shadow">
+      <h3 class="fw-bolder mb-3" style="font-family: 'IBMPlexSansKR-Regular', sans-serif;"><i class="bi bi-file-earmark-text"></i>&nbsp; 결재 문서</h3>
+      <b-card class="container shadow" style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">
         <div class="mx-1 my-3">
-          <div class="text-success" v-if="state.approval['approvalStatus'] === 'A'"> <h5 >승인됨</h5></div>
-          <div class="text-danger" v-else-if="state.approval['approvalStatus'] === 'D'"> <h5 >반려됨</h5></div>
-          <div class="text-warning" v-else-if="state.approval['cancelYn'] === 'Y'"> <h5 >취소</h5></div>
-          <div class="text-muted" v-else> <h5 >결재 대기중</h5></div>
+          <div class="text-success" v-if="state.approval['approvalStatus'] === 'A'"> <h5 style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">승인됨</h5></div>
+          <div class="text-danger" v-else-if="state.approval['approvalStatus'] === 'D'"> <h5 style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">반려됨</h5></div>
+          <div class="text-warning" v-else-if="state.approval['cancelYn'] === 'Y'"> <h5 style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">취소</h5></div>
+          <div class="text-muted" v-else> <h5 style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">결재 대기중</h5></div>
         </div>
         <div class="title">
           <h1 v-if="typeId === '1'">출장 신청서</h1>
@@ -58,21 +58,21 @@
           <h1 v-if="typeId === '4'">휴가 신청서</h1>
         </div>
         <div class="text-secondary mb-4">{{ state.approval['writeTime'] }} &nbsp;작성</div>
-        <div v-if="state.approval && state.approval.user && state.approval.user.duties">
-          <div href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true" style="margin-bottom: -10px;">
+        <div v-if="state.approval && state.approval.user && state.approval.user.duties" style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">
+          <div href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true" style="margin-bottom: -10px; font-family: 'IBMPlexSansKR-Regular', sans-serif;">
             <template v-if="state.user['profilePath'] === null"><img :src="defaultProfileImage" alt="profile" width="50" height="50" class="rounded-circle flex-shrink-0"></template>
             <template v-else><img :src="state.user['profilePath']" alt="profile" width="50" height="50" class="rounded-circle flex-shrink-0"/></template>
             <div class="d-flex gap-2 w-100 justify-content-between">
               <div class="mt-1">
-                <h5 class="fw-bolder mb-0">&nbsp;&nbsp;&nbsp;{{ state.user['name'] }}</h5>
+                <h5 class="fw-bolder mb-0" style="font-family: 'IBMPlexSansKR-Regular', sans-serif;">&nbsp;&nbsp;&nbsp;{{ state.user['name'] }}</h5>
                 <p class="mb-0 opacity-75">&nbsp;&nbsp;&nbsp;{{ state.user['team'].teamName }} / {{ state.user['duties'].dutiesName }}</p>
               </div>
             </div>
           </div>
           <hr style="border-top: 2px dotted #000; margin-bottom: 20px;">
           <div style="margin-left: 10px;">
-            <h6 class="mb-3"><strong><i class="bi fs-5 bi-envelope"></i>&nbsp; &nbsp;</strong> {{ state.user['email'] }}</h6>
-            <h6><strong><i class="bi fs-5 bi-telephone"></i>&nbsp; &nbsp; </strong> {{ state.user['callNumber'] }}</h6>
+            <h6 class="mb-3"><strong style="font-family: 'IBMPlexSansKR-Regular', sans-serif;"><i class="bi fs-5 bi-envelope"></i>&nbsp; &nbsp;</strong> {{ state.user['email'] }}</h6>
+            <h6><strong style="font-family: 'IBMPlexSansKR-Regular', sans-serif;"><i class="bi fs-5 bi-telephone"></i>&nbsp; &nbsp; </strong> {{ state.user['callNumber'] }}</h6>
           </div>
           <hr style="border-top: 2px dotted #000; margin-top: 20px; margin-bottom: 30px;">
         </div>
@@ -89,7 +89,6 @@
   display:grid;
   grid-template-columns: 10% 80% 10%;
   grid-template-rows: 13% 80% 7%;
-  
 }
 
 .all h3 {
@@ -112,5 +111,6 @@
   margin-bottom: 0;
   font-size: 23px;
   font-weight: 600;
+  font-family: 'IBMPlexSansKR-Regular', sans-serif;
 }
 </style>
