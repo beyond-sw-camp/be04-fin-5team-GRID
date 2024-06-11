79f54406d389d8a9828de1483350fa6749f46f01
LeeYewon <onelee521@gmail.com>
2024-06-12 03:05:19 +0900

    Fix: 출퇴근, 업적 평가 테스트코드 추가

--- grid/src/test/java/org/highfives/grid/performance_review/query/service/TotalPerformanceReviewServiceImplTest.java
+++ grid/src/test/java/org/highfives/grid/performance_review/query/service/TotalPerformanceReviewServiceImplTest.java
@@ -1,57 +1,57 @@
-//package org.highfives.grid.performance_review.query.service;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//class TotalPerformanceReviewServiceImplTest {
-//
-//    @Autowired
-//    private TotalPerformanceReviewServiceImpl totalPerformanceReviewService;
-//
-//    @DisplayName("종합 평가 평가대상자 기준으로 조회")
-//    @Test
-//    public void findTotalByRevieweeId(){
-//        int employeeId = 11;
-//
-//        assertNotNull(
-//                totalPerformanceReviewService.findTotalByRevieweeId(employeeId)
-//        );
-//
-//    }
-//
-//    @DisplayName("종합 평가 평가자 기준으로 조회")
-//    @Test
-//    public void findTotalByReviewerId(){
-//        int employeeId = 10;
-//
-//        assertNotNull(
-//                totalPerformanceReviewService.findTotalByReviewerId(employeeId)
-//        );
-//
-//    }
-//
-//    @DisplayName("종합 평가 전체 조회")
-//    @Test
-//    public void findAllTotal(){
-//
-//        assertNotNull(
-//                totalPerformanceReviewService.findAllTotal()
-//        );
-//
-//    }
-//
-//    @DisplayName("종합 평가 하나 조회")
-//    @Test
-//    public void findTotalById(){
-//        int id = 1;
-//
-//        assertNotNull(
-//                totalPerformanceReviewService.findTotalById(id)
-//        );
-//    }
-//}
+package org.highfives.grid.performance_review.query.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TotalPerformanceReviewServiceImplTest {
+
+    @Autowired
+    private TotalPerformanceReviewServiceImpl totalPerformanceReviewService;
+
+    @DisplayName("종합 평가 평가대상자 기준으로 조회")
+    @Test
+    public void findTotalByRevieweeId(){
+        int employeeId = 5;
+
+        assertNotNull(
+                totalPerformanceReviewService.findTotalByRevieweeId(employeeId)
+        );
+
+    }
+
+    @DisplayName("종합 평가 평가자 기준으로 조회")
+    @Test
+    public void findTotalByReviewerId(){
+        int employeeId = 3;
+
+        assertNotNull(
+                totalPerformanceReviewService.findTotalByReviewerId(employeeId)
+        );
+
+    }
+
+    @DisplayName("종합 평가 전체 조회")
+    @Test
+    public void findAllTotal(){
+
+        assertNotNull(
+                totalPerformanceReviewService.findAllTotal()
+        );
+
+    }
+
+    @DisplayName("종합 평가 하나 조회")
+    @Test
+    public void findTotalById(){
+        int id = 1;
+
+        assertNotNull(
+                totalPerformanceReviewService.findTotalById(id)
+        );
+    }
+}
